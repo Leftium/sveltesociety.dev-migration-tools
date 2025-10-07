01b095ec44b010b0b55ae473679995c9cbd3aa2d
Leftium <381217+Leftium@users.noreply.github.com>
2025-10-07 00:14:31 -0500

    feat: load github url data and render as list.
    Data from: https://github.com/svelte-society/sveltesociety.dev/blob/legacy/src/lib/data/github.json

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import githubJson from '$lib/data/github.json';
+</script>
+
+{#each Object.keys(githubJson) as githubUrl, index}
+	{@const ownerRepo = githubUrl.replace('https://github.com/', '')}
+	<div>
+		{index + 1} <a href={githubUrl} target="_blank">{ownerRepo}</a>
+	</div>
+{/each}
