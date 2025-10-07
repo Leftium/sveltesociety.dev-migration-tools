8342826fdcb0c08809132cf87f676cd55390336e
Leftium <381217+Leftium@users.noreply.github.com>
2025-10-07 00:25:05 -0500

    feat: add checkbox and grid styling.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,52 @@
 	import githubJson from '$lib/data/github.json';
 </script>
 
-{#each Object.keys(githubJson) as githubUrl, index}
-	{@const ownerRepo = githubUrl.replace('https://github.com/', '')}
-	<div>
-		{index + 1} <a href={githubUrl} target="_blank">{ownerRepo}</a>
-	</div>
-{/each}
+<div class="grid-container">
+	{#each Object.keys(githubJson) as githubUrl, index}
+		{@const ownerRepo = githubUrl.replace('https://github.com/', '')}
+		<div class="grid-row">
+			<div class="index">{index + 1}</div>
+			<div class="checkbox">
+				<input type="checkbox" />
+			</div>
+			<div class="repo">
+				<a href={githubUrl} target="_blank">{ownerRepo}</a>
+			</div>
+		</div>
+	{/each}
+</div>
+
+<style>
+	.grid-container {
+		display: grid;
+		gap: 0;
+	}
+
+	.grid-row {
+		display: grid;
+		grid-template-columns: 4rem 3rem 1fr;
+		align-items: center;
+		border-bottom: 1px solid var(--pico-muted-border-color);
+		padding: 0.5rem 0;
+	}
+
+	.grid-row:last-child {
+		border-bottom: none;
+	}
+
+	.index {
+		text-align: right;
+		padding-right: 1rem;
+		color: var(--pico-muted-color);
+	}
+
+	.checkbox {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.repo {
+		padding-left: 0.5rem;
+	}
+</style>
