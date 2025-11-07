c09bfc0cec08471c4d6b4a1772cdc783cf7670c2
John-Kim Murphy <381217+Leftium@users.noreply.github.com>
2025-11-07 15:06:46 -0600

    feat: add Svelte Society youtube link.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <ul>
 	<li><a href="https://sveltesociety.dev/admin">sveltesociety.dev/admin</a></li>
+	<li><a href="https://www.youtube.com/@SvelteSociety/videos">SvelteSociety@youtube</a></li>
 	<li><a href="https://discord.com/channels/@me/1420491703969648670">Kev@Discord</a></li>
 	<li>
 		<a href="https://discord.com/channels/457912077277855764/1186398899225759744"
